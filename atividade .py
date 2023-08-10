8e83f3897472ecae4dc05e436ff9c4de59565db1
celso eduardo <celsoeduardobentopugim@gmail.com>
2023-08-10 00:02:40 -0300

    first commit

---
+++ atividade .py
@@ -0,0 +1,8 @@
+nome = 'celso eduardo bento pugim'
+idade = '22'
+altura = '1,71'
+peso = '60'
+print('nome:'+ nome)
+print('idade:'+ str (idade))
+print('altura:'+str (altura))
+print('peso:'+str (peso))
